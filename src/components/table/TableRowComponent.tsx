09f85678ec6c7fa11be52e230f07e0091327e881
Tamara <Oxian66@mail.ru>
2022-12-07 15:25:59 +0300

    fix textfield size

--- src/components/table/TableRowComponent.tsx
+++ src/components/table/TableRowComponent.tsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
-import { Props, TableData, UserInput } from '../interfaces';
+import React, { useState } from "react";
+import { Props, TableData, UserInput } from "../interfaces";
+import "./Table.scss";
 import {
   TableRow,
   TableCell,
   SpeedDial,
   SpeedDialAction,
   TextField,
-} from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
-import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
-
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import FolderOpenOutlinedIcon from "@mui/icons-material/FolderOpenOutlined";
 
 const TableRowComponent = (props: Props) => {
   const [userInput, setUserInput] = useState<UserInput | null>(null);
   const [editMode, setEditMode] = useState<boolean>(false);
 
   const actions = [
-    { icon: <FolderOpenOutlinedIcon color='success' />, name: 'Level 2', action: props.createRow },
-    { icon: <DescriptionOutlinedIcon />, name: 'Document', action: props.createRow },
-    { icon: <DeleteIcon color='error'/>, name: 'Delete', action: props.handleDelete },
+    {
+      icon: <FolderOpenOutlinedIcon color="success" />,
+      name: "Level 2",
+      action: props.createRow,
+    },
+    {
+      icon: <DescriptionOutlinedIcon />,
+      name: "Document",
+      action: props.createRow,
+    },
+    {
+      icon: <DeleteIcon color="error" />,
+      name: "Delete",
+      action: props.handleDelete,
+    },
   ];
   const actionSize = {
     width: 0,
     height: 0,
-    m: '1em',
-  }
+    m: "1em",
+  };
 
   const handleEditRow = () => {
-    setEditMode(true);
+    setEditMode(!editMode);
     console.log(editMode);
   };
 
@@ -38,71 +50,95 @@ const TableRowComponent = (props: Props) => {
     //   [e.target.name as string]:[e.target.value]
     // });
   };
-  console.log('props', props);
-  
+  console.log("props", props);
+
   return (
     <>
       {editMode ? (
-        <TableRow>
-          <TableCell>
+        <TableRow
+          sx={{
+            "& label.Mui-focused": {
+              color: "#71717A",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#71717A",
+              },
+              "&:hover fieldset": {
+                borderColor: "#71717A",
+              },
+            },
+          }}
+        >
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
             <SpeedDial
-              ariaLabel='level'
-              icon={<FolderOpenOutlinedIcon />}
-              direction='right'
-              sx={{ '& .MuiFab-primary': { width: 0, height: 0 } }}
+              ariaLabel="level"
+              icon={<FolderOpenOutlinedIcon color="primary" />}
+              direction="right"
+              sx={{ "& .MuiFab-primary": { width: 0, height: 0 } }}
             >
               {actions.map((action) => (
                 <SpeedDialAction
                   key={action.name}
                   icon={action.icon}
                   tooltipTitle={action.name}
-                  sx={{width: 0, height: 0}}
+                  sx={actionSize}
                   onClick={() => action.action(props.data.id)}
                 />
               ))}
             </SpeedDial>
           </TableCell>
-          <TableCell >
-            <TextField />
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            <TextField size='small'/>
           </TableCell>
-          <TableCell>
-            <TextField />
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            <TextField size='small'/>
           </TableCell>
-          <TableCell>
-            <TextField />
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            <TextField size='small'/>
           </TableCell>
-          <TableCell>
-            <TextField />
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            <TextField size='small'/>
           </TableCell>
-          <TableCell>
-            <TextField />
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            <TextField size='small'/>
           </TableCell>
         </TableRow>
       ) : (
-        <TableRow onDoubleClick={handleEditRow} >
-          <TableCell sx={{pt: 0, pb: 0}}>
-              <SpeedDial
-                ariaLabel='level'
-                icon={<FolderOpenOutlinedIcon color='primary' />}
-                direction='right'
-                sx={{ '& .MuiFab-primary': { width: 0, height: 0 } }}
-              >
-                {actions.map((action) => (
-                  <SpeedDialAction
-                    key={action.name}
-                    icon={action.icon}
-                    tooltipTitle={action.name}
-                    sx={actionSize}
-                    onClick={() => action.action(props.data.id)}
-                  />
-                ))}
-              </SpeedDial>
-          </TableCell >
-          <TableCell sx={{color: 'white', pt: 0, pb: 0 }}>{props.data.rowName}</TableCell>
-          <TableCell sx={{color: 'white', pt: 0, pb: 0}}>{props.data.salary}</TableCell>
-          <TableCell sx={{color: 'white', pt: 0, pb: 0}}>{props.data.equipmentCosts}</TableCell>
-          <TableCell sx={{color: 'white', pt: 0, pb: 0}}>{props.data.overheads}</TableCell>
-          <TableCell sx={{color: 'white', pt: 0, pb: 0}}>{props.data.estimatedProfit}</TableCell>
+        <TableRow onDoubleClick={handleEditRow}>
+          <TableCell sx={{ pt: 0, pb: 0 }}>
+            <SpeedDial
+              ariaLabel="level"
+              icon={<FolderOpenOutlinedIcon color="primary" />}
+              direction="right"
+              sx={{ "& .MuiFab-primary": { width: 0, height: 0 } }}
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                  sx={actionSize}
+                  onClick={() => action.action(props.data.id)}
+                />
+              ))}
+            </SpeedDial>
+          </TableCell>
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            {props.data.rowName}
+          </TableCell>
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            {props.data.salary}
+          </TableCell>
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            {props.data.equipmentCosts}
+          </TableCell>
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            {props.data.overheads}
+          </TableCell>
+          <TableCell sx={{ color: "white", pt: 0, pb: 0 }}>
+            {props.data.estimatedProfit}
+          </TableCell>
         </TableRow>
       )}
     </>
